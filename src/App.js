922678974a3abf30e8e06739b36b1bd41bae5591
Abraham Choi <abraham.choi@icloud.com>
2021-04-03 21:57:33 -0700

    29min

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useInterval } from "./useInterval";
+import {
+  CANVAS_SIZE,
+  SNAKE_START,
+  APPLE_START,
+  SCALE,
+  SPEED,
+  DIRECTIONS,
+} from "./constants";
 
 const App = () => {
-  return <div>Snake Game</div>;
+  const canvasRef = useRef(null);
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [apple, setApple] = useState(APPLE_START);
+  const [dir, setDir] = useState([0, -1]);
+  const [speed, setSpeed] = useState(1000);
+  const [gameOver, setGameOver] = useState(false);
+
+  const startGame = () => {};
+  const endGame = () => {};
+  const moveSnake = ({ keyCode }) =>
+    keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
+  const createApple = () => {};
+  const checkCollision = () => {};
+  const checkApple = () => {};
+  const gameLoop = () => {
+    const snakeCopy = JSON.parse(JSON.stringify(snake));
+    const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
+    snakeCopy.unshift(newSnakeHead);
+    snakeCopy.pop(); // remove tail
+    setSnake(snakeCopy);
+  };
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
+    context.clearRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1]);
+    context.fillStyle = "pink";
+    snake.forEach(([x, y]) => {
+      context.fillRect(x, y, 1, 1);
+    });
+    context.fillStyle = "lightblue";
+    context.fillRect(apple[0], apple[1], 1, 1);
+  }, [snake, apple, gameOver]);
+
+  useInterval(() => gameLoop(), speed);
+
+  return (
+    <div role="button" tabIndex="0" onKeyDown={(e) => moveSnake(e)}>
+      <canvas
+        style={{ border: "1px solid black" }}
+        ref={canvasRef}
+        width={`${CANVAS_SIZE[0]}px`}
+        height={`${CANVAS_SIZE[1]}px`}
+      />
+      {gameOver && <div>GAME OVER!</div>}
+      <button onClick={startGame}>Start Game</button>
+    </div>
+  );
 };
 
 export default App;
